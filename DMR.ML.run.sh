32f36e9e79602f45a9ece3845364e7ada1594a35
nickcheng96 <76527949+nickcheng96@users.noreply.github.com>
2021-12-08 15:36:50 -0500

    Update DMR.ML.run.sh

--- DMR.ML.run.sh
+++ DMR.ML.run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /oicr/local/Modules/default/init/bash
-scriptdir='/.mounts/labs/awadallalab/private/ncheng/cfmedip_data/cptp_samples/cancer_analysis/dmr_analysis/breast/1/'
+scriptdir='/path/to/script/directory/'
 cd $scriptdir
-./breast_reads.readno_run.seedno_run.R
+./breast_seedno_run.R
