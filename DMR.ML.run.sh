d7d42ee179e9b629dfa419d565b617549df9dab5
nickcheng96 <76527949+nickcheng96@users.noreply.github.com>
2021-12-08 15:27:33 -0500

    Create DMR.ML.run.sh

---
+++ DMR.ML.run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /oicr/local/Modules/default/init/bash
+scriptdir='/.mounts/labs/awadallalab/private/ncheng/cfmedip_data/cptp_samples/cancer_analysis/dmr_analysis/breast/1/'
+cd $scriptdir
+./breast_reads.readno_run.seedno_run.R
